324b525476eda2496843d9f8d18c222739bc1699
zorgonide <ammar1998@gmail.com>
2023-07-07 21:01:04 +0100

    changed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/favicon.ico",
+        icon: "src/images/favicoon.png",
       },
     },
   ],
