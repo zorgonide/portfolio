45e5e36ec28cfb20403075e4e3f3e8cf7a23a2ca
zorgonide <ammar1998@gmail.com>
2025-09-26 16:53:39 +0100

    trying to fix a bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export const Head = () => (
     <meta name='description' content='The portfolio of Ammar Khurshid, a developer.' />
     <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' />
     <script src='https://www.google.com/recaptcha/api.js' async defer></script>
-    <script type='text/javascript'>var onloadCallback = function() {alert('grecaptcha is ready!')};</script>
+    <script type='text/javascript'>var onloadCallback = function() {}</script>
   </>
 );
 export default IndexPage;
